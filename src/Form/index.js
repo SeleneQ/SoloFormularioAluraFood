ab6599c10f262a232cb5876ead7fc501e23d3a95
lunaselenebebe <lunaselenebebe@hotmail.com>
2023-09-29 13:17:13 -0600

    Solo formulario Alura food

---
+++ src/Form/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Box, Typography } from "@mui/material";
+import { LogoSpace, FormSpace, Img } from "./styles";
+import DatosUsuario from "./DatosUsuario";
+import DatosPersonales from "./DatosPersonales";
+import DatosEntrega from "./DatosEntrega";
+import Complete from "./Complete";
+import Stepper from "../Stepper";
+
+const Form = () => {
+  const [step, setStep] = useState(0);
+
+  //step = 0 --> <DatosUsuario />
+  //step = 1 --> <DatosPersonales />
+  //step = 2 --> <DatosEntrega />
+  //step = 3 --> <Complete />
+
+  const updateStep = (step) => {
+    console.log("actualizar paso", step);
+    setStep(step);
+  };
+
+  const steps = {
+    0: <DatosUsuario updateStep={updateStep} />,
+    1: <DatosPersonales updateStep={updateStep} />,
+    2: <DatosEntrega updateStep={updateStep} />,
+    3: <Complete />,
+  };
+
+  return (
+    <Box
+      sx={{
+        padding: "30px",
+        display: "flexbox",
+        flexDirection: "column",
+      }}
+    >
+      <LogoSpace>
+        <Img src={"/favicon.png"} />
+        <Typography variant="h3">AluraFood</Typography>
+      </LogoSpace>
+      <FormSpace>
+        {step < 3 && <Stepper step={step} />}
+        {/* <DatosUsuario />
+        <DatosPersonales />
+        <DatosEntrega /> */}
+        {steps[step]}
+      </FormSpace>
+    </Box>
+  );
+};
+
+export default Form;
